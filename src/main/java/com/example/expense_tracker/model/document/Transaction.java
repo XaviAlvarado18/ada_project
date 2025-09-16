774cd0c42771308f4ad9a437f5b6319b23e9ca93
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2025-09-15 23:30:22 -0600

    feat: add document transaction

--- src/main/java/com/example/expense_tracker/model/document/Transaction.java
+++ src/main/java/com/example/expense_tracker/model/document/Transaction.java
@@ -1,4 +1,17 @@
 package com.example.expense_tracker.model.document;
 
+import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document(collection = "transactions")
 public class Transaction {
+    @Id
+    private String id;
+    private Long userId;
+    private String type; // INCOME | EXPENSE
+    private String category;
+    private Double amount;
+    private LocalDate date;
 }
