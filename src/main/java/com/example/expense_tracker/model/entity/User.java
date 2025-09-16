6d19dc7f94b9a3ea4b819a0053fbf9fb4e6d432d
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2025-09-15 23:29:35 -0600

    feat: add user entity

--- src/main/java/com/example/expense_tracker/model/entity/User.java
+++ src/main/java/com/example/expense_tracker/model/entity/User.java
@@ -1,4 +1,15 @@
 package com.example.expense_tracker.model.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
 }
