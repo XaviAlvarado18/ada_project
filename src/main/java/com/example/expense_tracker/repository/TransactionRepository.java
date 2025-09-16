d9543b2672a18ce0cf3a348e22bef8e7c5140caf
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2025-09-15 23:34:29 -0600

    chore: add interfaces

--- src/main/java/com/example/expense_tracker/repository/TransactionRepository.java
+++ src/main/java/com/example/expense_tracker/repository/TransactionRepository.java
@@ -1,4 +1,10 @@
 package com.example.expense_tracker.repository;
 
-public class TransactionRepository {
+import com.example.expense_tracker.model.document.Transaction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends MongoRepository<Transaction, String> {
+    List<Transaction> findByUserId(Long userId);
 }
--- src/main/java/com/example/expense_tracker/repository/UserRepository.java
+++ src/main/java/com/example/expense_tracker/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.expense_tracker.repository;
 
-public class UserRepository {
+import com.example.expense_tracker.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
